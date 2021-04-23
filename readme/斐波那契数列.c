c592309dabb3a52ee24539f7abff76b5fca86357
mignon <2286382103@qq.com>
2021-04-23 18:27:28 +0800

    第二次上传

---
+++ readme/斐波那契数列.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int n1=1,n2=1,n3;
+	int cnt;
+	printf("%10d%10d",n1,n2);
+	for(cnt=3;cnt<=40;cnt++){
+		n3=n1+n2;
+		printf("%10d",n3);
+		n1=n2;
+		n2=n3;
+		if(cnt%4==0){
+			printf("\n");
+		}
+	}
+	return 0;
+}
